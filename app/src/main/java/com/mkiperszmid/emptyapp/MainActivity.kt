89a0191930e0cc445c9b91e1a78de1c5b1063b15
Martin Kiperszmid <MKiperszmid@gmail.com>
2023-09-15 16:04:30 -0300

    Usar CameraX en Jetpack Compose

--- app/src/main/java/com/mkiperszmid/emptyapp/MainActivity.kt
+++ app/src/main/java/com/mkiperszmid/emptyapp/MainActivity.kt
@@ -3,15 +3,11 @@ package com.mkiperszmid.emptyapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import com.mkiperszmid.emptyapp.home.HomeScreen
-import com.mkiperszmid.emptyapp.home.HomeViewModel
 import com.mkiperszmid.emptyapp.ui.theme.EmptyAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,14 +19,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background,
                 ) {
-                    val viewModel by viewModels<HomeViewModel>(factoryProducer = {
-                        object : ViewModelProvider.Factory {
-                            override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                                return HomeViewModel() as T
-                            }
-                        }
-                    })
-                    HomeScreen(viewModel = viewModel)
+                    HomeScreen()
                 }
             }
         }
--- app/src/main/java/com/mkiperszmid/emptyapp/home/HomeScreen.kt
+++ app/src/main/java/com/mkiperszmid/emptyapp/home/HomeScreen.kt
@@ -1,10 +1,95 @@
 package com.mkiperszmid.emptyapp.home
 
+import android.Manifest
+import android.view.ViewGroup
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.view.LifecycleCameraController
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberPermissionState
+import java.io.File
+import java.util.concurrent.Executor
+
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun HomeScreen() {
+    val permissionState = rememberPermissionState(permission = Manifest.permission.CAMERA)
+
+    val context = LocalContext.current
+    val cameraController = remember {
+        LifecycleCameraController(context)
+    }
+    val lifecycle = LocalLifecycleOwner.current
+
+    LaunchedEffect(Unit) {
+        permissionState.launchPermissionRequest()
+    }
+    Scaffold(modifier = Modifier.fillMaxSize(), floatingActionButton = {
+        FloatingActionButton(onClick = {
+            val executor = ContextCompat.getMainExecutor(context)
+            takePicture(cameraController, executor)
+        }) {
+            Text(text = "Camara!")
+        }
+    }) {
+        if (permissionState.status.isGranted) {
+            CamaraComposable(cameraController, lifecycle, modifier = Modifier.padding(it))
+        } else {
+            Text(text = "Permiso Denegado!", modifier = Modifier.padding(it))
+        }
+    }
+}
+
+private fun takePicture(cameraController: LifecycleCameraController, executor: Executor) {
+    val file = File.createTempFile("imagentest", ".jpg")
+    val outputDirectory = ImageCapture.OutputFileOptions.Builder(file).build()
+    cameraController.takePicture(
+        outputDirectory,
+        executor,
+        object : ImageCapture.OnImageSavedCallback {
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                println(outputFileResults.savedUri)
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                println()
+            }
+        },
+    )
+}
 
 @Composable
-fun HomeScreen(
-    viewModel: HomeViewModel,
+fun CamaraComposable(
+    cameraController: LifecycleCameraController,
+    lifecycle: LifecycleOwner,
+    modifier: Modifier = Modifier,
 ) {
-    val state = viewModel.state
+    cameraController.bindToLifecycle(lifecycle)
+    AndroidView(modifier = modifier, factory = { context ->
+        val previewView = PreviewView(context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT,
+            )
+        }
+        previewView.controller = cameraController
+
+        previewView
+    })
 }
